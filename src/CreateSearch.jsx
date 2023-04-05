f51d733c26b7fd19f52b7450579cd927ba02dc21
Zuhair <84449586+MohammedZuhairAhmed@users.noreply.github.com>
2023-04-05 21:08:21 +0530

    Update CreateSearch.jsx

--- src/CreateSearch.jsx
+++ src/CreateSearch.jsx
@@ -18,7 +18,7 @@ const CreateSearch = () => {
     },[]);     // eslint-disable-line react-hooks/exhaustive-deps
 
     async function requestList(){
-        const res = await fetch(`http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`);
+        const res = await fetch(`https://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`);
         const json = await res.json();
         setPets(json.pets);
 
